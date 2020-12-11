d3f069d765ad453dfc2a4050d1bed9a2de6c1b69
romain.aury <romain.aury@hotmail.com>
2020-12-11 14:08:28 +0100

    fix #1.3: Add Pie Chart in Pattern

--- src/main/java/observer/pattern/BarChartObserver.java
+++ src/main/java/observer/pattern/BarChartObserver.java
@@ -40,28 +40,26 @@ public class BarChartObserver extends JPanel implements Observer {
 	 */
 	public void paint(Graphics g) {
 		super.paint(g);
-		LayoutConstants.paintBarChartOutline(g, this.courseData.size());
+		int radius = 100;
+	
+		//first compute the total number of students
+		double total = 0.0;
 		for (int i = 0; i < courseData.size(); i++) {
-			CourseRecord record = (CourseRecord) courseData.elementAt(i);
-			g.setColor(LayoutConstants.courseColours[i]);
-			g.fillRect(
-					LayoutConstants.xOffset + (i + 1)
-							* LayoutConstants.barSpacing + i
-							* LayoutConstants.barWidth, LayoutConstants.yOffset
-							+ LayoutConstants.graphHeight
-							- LayoutConstants.barHeight
-							+ 2
-							* (LayoutConstants.maxValue - record
-									.getNumOfStudents()),
-					LayoutConstants.barWidth, 2 * record.getNumOfStudents());
-			g.setColor(Color.red);
-			g.drawString(record.getName(),
-					LayoutConstants.xOffset + (i + 1)
-							* LayoutConstants.barSpacing + i
-							* LayoutConstants.barWidth, LayoutConstants.yOffset
-							+ LayoutConstants.graphHeight + 20);
+			total += courseData.elementAt(i).getNumOfStudents();
+		}
+		//if total == 0 nothing to draw
+		if (total != 0) {
+			double startAngle = 0.0;
+			for (int i = 0; i < courseData.size(); i++) {
+				double ratio = (courseData.elementAt(i).getNumOfStudents() / total) * 360.0;
+				//draw the arc
+				g.setColor(LayoutConstants.courseColours[i%LayoutConstants.courseColours.length]);
+				g.fillArc(LayoutConstants.xOffset, LayoutConstants.yOffset , 2 * radius, 2 * radius, (int) startAngle, (int) ratio);
+				startAngle += ratio;
+			}
 		}
 	}
+	
 
 	/**
 	 * Informs this observer that the observed CourseData object has changed
